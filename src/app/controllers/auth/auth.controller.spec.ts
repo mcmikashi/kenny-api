20289163bb78fff8d405b54bae0a5e159ef0d3f5
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-12-13 18:45:42 -0300

    Add the signup page
    * User can create account with a valid email and password then get is token
    
    Close #1

--- src/app/controllers/auth/auth.controller.spec.ts
+++ src/app/controllers/auth/auth.controller.spec.ts
@@ -1,30 +1,103 @@
 // std
-import { ok, strictEqual } from 'assert';
+import { ok, strictEqual, notEqual, rejects } from 'assert';
 
 // 3p
 import { Context, createController, getHttpMethod, getPath, isHttpResponseOK } from '@foal/core';
+import { DataSource } from 'typeorm';
 
 // App
 import { AuthController } from './auth.controller';
+import { createDataSource } from '../../../db';
+import { User } from '../../entities';
 
 describe('AuthController', () => {
 
+  let dataSource: DataSource;
   let controller: AuthController;
+
+  before(async () => {
+    dataSource = createDataSource();
+    await dataSource.initialize();
+  });
+
+  // Close the database connection after running all the tests whether they succeed or failed.
+  after(async () => {
+    if (dataSource) {
+      await dataSource.close();
+    }
+  });
 
   beforeEach(() => controller = createController(AuthController));
 
-  describe('has a "foo" method that', () => {
-
-    it('should handle requests at GET /.', () => {
-      strictEqual(getHttpMethod(AuthController, 'foo'), 'GET');
-      strictEqual(getPath(AuthController, 'foo'), '/');
-    });
-
-    it('should return an HttpResponseOK.', () => {
-      const ctx = new Context({});
-      ok(isHttpResponseOK(controller.foo(ctx)));
-    });
-
+  it('should handle requests POST request', () => {
+    strictEqual(getHttpMethod(AuthController, 'signup'), 'POST');
+    strictEqual(getPath(AuthController, 'signup'), '/signup');
   });
 
+  it('should return an HttpResponseOK, an user should be created and jwt should be set', async () => {
+    const ctx = new Context({
+      body: {
+        'email': 'jhondoe@test.com',
+        'username': 'jhone2',
+        'password': 'thescsuperscretpassword123',
+      }
+    });
+    const response = await controller.signup(ctx);
+    // Check that  the auth jwt token is set
+    notEqual(response.getCookie('auth'), null, 'an user should have an auth token');
+    ok(isHttpResponseOK(response), 'response should be an instance of HttpResponseOK.');
+    // Check that the user is created
+    const user = await User.findOneBy({ email: ctx.request.body.email });
+    strictEqual(user?.username, ctx.request.body.username, 'an user should be created with the good username');
+    notEqual(user?.password, ctx.request.body.password, 'an user should have a crypted password');
+  });
+
+  it('should not return an QueryFailedError when we does not send username and email', async () => {
+    const ctx = new Context({
+      body: {
+        'password': 'thescsuperscretpassword123',
+      }
+    });
+    await rejects(
+      async () => {
+        const response = await controller.signup(ctx);
+      },
+      {
+        name: 'QueryFailedError',
+      });
+  });
+
+  it('should not return an QueryFailedError when we does not send a valid email', async () => {
+    const ctx = new Context({
+      body: {
+        'email': 'jhondoe.com',
+        'username': 'jhone2',
+        'password': 'thescsuperscretpassword123',
+      }
+    });
+    await rejects(
+      async () => {
+        const response = await controller.signup(ctx);
+      },
+      {
+        name: 'QueryFailedError',
+      });
+  });
+
+  it('should not return an QueryFailedError when we does not send a valid username', async () => {
+    const ctx = new Context({
+      body: {
+        'email': 'jhondoe@test.com',
+        'username': 'jd',
+        'password': 'thescsuperscretpassword123',
+      }
+    });
+    await rejects(
+      async () => {
+        const response = await controller.signup(ctx);
+      },
+      {
+        name: 'QueryFailedError',
+      });
+  });
 });
--- src/app/controllers/auth/auth.controller.ts
+++ src/app/controllers/auth/auth.controller.ts
@@ -1,10 +1,46 @@
-// Core
-import { Context, HttpResponseOK, Post } from '@foal/core';
+import { Context, hashPassword, HttpResponseOK, Post, ValidateBody } from '@foal/core';
+import { getSecretOrPrivateKey, setAuthCookie } from '@foal/jwt';
+import { sign } from 'jsonwebtoken';
+import { promisify } from 'util';
+import { User } from '../../entities';
+
+const credentialsSchema = {
+  additionalProperties: false,
+  properties: {
+    email: { type: 'string', format: 'email' },
+    username: { type: 'string' },
+    password: { type: 'string' }
+  },
+  required: ['email', 'password'],
+  type: 'object',
+};
 
 export class AuthController {
 
-  @Post('/login')
-  async login(ctx: Context) {
-    return new HttpResponseOK('');
+  @Post('/signup')
+  @ValidateBody(credentialsSchema)
+  async signup(ctx: Context) {
+    const user = new User();
+    user.email = ctx.request.body.email;
+    user.username = ctx.request.body.username;
+    user.password = await hashPassword(ctx.request.body.password);
+    await user.save();
+
+    const response = new HttpResponseOK();
+    await setAuthCookie(response, await this.createJWT(user));
+    return response;
+  }
+
+  private async createJWT(user: User): Promise<string> {
+    const payload = {
+      email: user.email,
+      id: user.id,
+    };
+
+    return promisify(sign as any)(
+      payload,
+      getSecretOrPrivateKey(),
+      { subject: user.id.toString() }
+    );
   }
 }
